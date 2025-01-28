fafaaf2fe5aca548480804494c3e251523161b3c
PranavParalkar <pranavparalkar21@gmail.com>
2025-01-28 19:08:18 +0530

    28/01/2024
    Moving Platform and Killzone

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 130
-const JUMP_VELOCITY = -350
+const SPEED = 300
+const JUMP_VELOCITY = -550
 
 
 func _physics_process(delta: float) -> void:
@@ -10,12 +10,10 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
